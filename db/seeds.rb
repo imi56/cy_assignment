521d94e9a477d9c017ef095cd6f68b57da1fcac3
Imran <zaran.56@gmail.com>
2020-02-09 19:36:43 +0530

    Fixed seed data
     - since seed data is not run on any database yet changed it directly
     - If any database aleady had it, rake task would have been needed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,22 @@
 # create data
 
 Item.insert_all!([
-  { name: "A", price: "30" },
-  { name: "B", price: "20" },
-  { name: "C", price: "50" },
-  { name: "D", price: "50" },
+  { name: "A", price: 30 },
+  { name: "B", price: 20 },
+  { name: "C", price: 50 },
+  { name: "D", price: 15 },
 ])
 
+# if seed date is already there in anyone's database, we need a rake task to fix the data
 Promotion.insert_all!([
-  {category: "item_amount", rule: {quantity: 3, unit: "amount", value: 15}, active: true},
-  {category: "item_amount", rule: {quantity: 2, unit: "amount", value: 5}, active: true},
-  {category: "total_amount", rule: {threshost_amount: 150, unit: "amount", value: 20}, active: true},
-  {category: "item_percentage", rule: {quantity: 3, unit: "percentage", value: 10}, active: false},
+  { category: "item", rule: { quantity: 3, unit: Promotion::AMOUNT, value: 15 }, active: true },
+  { category: "item", rule: { quantity: 2, unit: Promotion::AMOUNT, value: 5 }, active: true },
+  { category: "item", rule: { quantity: 3, unit: Promotion::PERCENTAGE, value: 10 }, active: false },
+  { category: "total", rule: { threshost_amount: 150, unit: Promotion::AMOUNT, value: 20 }, active: true },
+  { category: "total", rule: { threshost_amount: 150, unit: Promotion::PERCENTAGE, value: 5 }, active: false },
 ])
 
 ItemPromotion.insert_all!([
-  {item_id: 1, promotion_id: 1},
-  {item_id: 2, promotion_id: 2}
+  { item_id: 1, promotion_id: 1 },
+  { item_id: 2, promotion_id: 2 }
 ])
